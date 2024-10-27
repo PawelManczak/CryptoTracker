203e86e29dd188bfa07182fb057233de996e1b4a
PawelManczak <pawel14522@gmail.com>
2024-10-27 21:20:10 +0100

    refactored

--- app/src/main/java/com/plcoding/cryptotracker/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/plcoding/cryptotracker/data/networking/RemoteCoinDataSource.kt
@@ -1,9 +1,9 @@
-import com.plcoding.core.data.networking.constructUrl
-import com.plcoding.core.data.networking.safeCall
-import com.plcoding.core.domain.CoinDataSource
-import com.plcoding.core.domain.util.NetworkError
-import com.plcoding.core.domain.util.Result
-import com.plcoding.core.domain.util.map
+import com.plcoding.cryptotracker.core.data.networking.constructUrl
+import com.plcoding.cryptotracker.core.data.networking.safeCall
+import com.plcoding.cryptotracker.core.domain.CoinDataSource
+import com.plcoding.cryptotracker.core.domain.util.NetworkError
+import com.plcoding.cryptotracker.core.domain.util.Result
+import com.plcoding.cryptotracker.core.domain.util.map
 import com.plcoding.cryptotracker.crypto.domain.Coin
 import com.plcoding.cryptotracker.data.mappers.toCoin
 import com.plcoding.cryptotracker.data.networking.dto.CoinsResponseDto
